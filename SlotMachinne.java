683abe9a73d4579c6015616cb27beb94d50f50af
Dulan Dhanush <dulandhanush0@gmail.com>
2025-07-04 13:11:14 +0530

    Update SlotMachinne.java

--- SlotMachinne.java
+++ SlotMachinne.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class SlotMachinne {
+public class SlotMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
